dedf56280de361c4ae7047de2529286cc24ac11c
Xello777 <al@i-projects.pro>
2025-06-15 18:57:57 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sprintbot.db")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 DEBUG_MODE = os.getenv("DEBUG", "False").lower() == "true"
